acfc2ce3f55a5c9c03e268ee4b11fe7bf4da0ea8
Gkxie <xieyu141419@jd.com>
2020-02-26 14:43:47 +0800

    fix: auto merge
    bot merge automatically PR when member of organization approved.

--- src/scripts/auto-merge.js
+++ src/scripts/auto-merge.js
@@ -3,22 +3,45 @@ const githubClient = require('../lib/github-client')
 const { createComment, merge } = require('../common/github')
 
 async function resolveEvent (data, owner, repo) {
+  /**
+   * @description 判断检查套件是否完成
+   * @param {Number} pull_number PR 序号
+   */
   const { number: pull_number } = data.pull_request
-
-  // 获取PR信息
   const { data: pullRequest } = await githubClient.pulls.get({
     owner,
     repo,
     pull_number
   })
-
-  // 判断是否可以合并
-  const { merged, mergeable, mergeable_state } = pullRequest
+  /**
+   * @description 判断检查套件是否完成
+   * @param {Boolean} merged PR是否可以合并
+   * @param {Number} mergeable PR是否已经合并
+   * @param {String} mergeable_state 当值为clean时merge按钮才能点击
+   * @param {String} sha PR的commit的头指针
+   */
+  const { merged, mergeable, mergeable_state, head: { sha } } = pullRequest
   if (merged || !mergeable || mergeable_state !== 'clean') {
     return
   }
+  /**
+   * @description 头指针被官方成员批准才能merge
+   * @param {String} commit_id reviewer审阅的commit的sha
+   * @param {String} state reviewer审阅的action,值为APPROVED时代表批准
+   * @param {String} author_association reviewer的身份，值为member时代表为组织成员
+   */
+  const { data: reviews } = await githubClient.pulls.listReviews({
+    owner,
+    repo,
+    pull_number
+  })
+  const approved = reviews.some(review => {
+    const { commit_id, state, author_association } = review
+    return (commit_id === sha && state === 'APPROVED' && author_association === 'MEMBER')
+  })
+  if (!approved) return
+  /** merge PR **/
   const [error] = await merge(owner, repo, pull_number, 'rebase')
-  // 合并成功之后留言，合并失败啥也不干。
   if (error) return
   createComment(owner, repo, pull_number, 'auto rebased ~!')
 }
